f2a2165274118de207389a60eac88f3d7d6f06fd
martinjiang8 <58094948+martinjiang8@users.noreply.github.com>
2019-11-23 14:57:47 -0500

    Add files via upload

--- install0.bat
+++ install0.bat
@@ -15,12 +15,14 @@ curl http://download.tensorflow.org/models/object_detection/faster_rcnn_inceptio
 "C:\Program Files\7-Zip\7z.exe" x  faster_rcnn_inception_v2_coco_2018_01_28.tar  -o./ -y -r
 
 "C:\Program Files\Git\bin\git.exe" clone https://github.com/EdjeElectronics/TensorFlow-Object-Detection-API-Tutorial-Train-Multiple-Objects-Windows-10.git
-
+SET src_folder=c:\tensorflow1\models\research\object_detection\TensorFlow-Object-Detection-API-Tutorial-Train-Multiple-Objects-Windows-10
+SET tar_folder=c:\tensorflow1\models\research\object_detection
+for /f "delims=|" %%a IN ('dir "%src_folder%" /b') do move %src_folder%\%%a %tar_folder%
 
 call conda create -y -n tensorflow1 pip python=3.5
 
 copy c:\tensorflow1.5\install1.bat c:\tensorflow1\install1.bat
-copy c:\tensorflow1.5\set_path.bat c:\tensorflow1\set_path.bat
+copy c:\tensorflow1.5\set_path1.bat c:\tensorflow1\set_path1.bat
 copy c:\tensorflow1.5\train.bat c:\tensorflow1\train.bat
 
 cd c:\tensorflow1
--- train.bat
+++ train.bat
@@ -1,5 +1,3 @@
-set PYTHONPATH=C:\tensorflow1\models;C:\tensorflow1\models\research;C:\tensorflow1\models\research\slim
-cd C:\tensorflow1\models\research\object_detection
 
 python generate_tfrecord.py --csv_input=images\train_labels.csv --image_dir=images\train --output_path=train.record
 python generate_tfrecord.py --csv_input=images\test_labels.csv --image_dir=images\test --output_path=test.record
