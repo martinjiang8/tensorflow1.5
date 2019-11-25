617ba375d6ef69b0a1541265ecafd7f1d1774685
martinjiang8 <58094948+martinjiang8@users.noreply.github.com>
2019-11-25 13:05:47 -0500

    change install0.bat

---
+++ install0.bat
@@ -0,0 +1,23 @@
+cd\
+md tensorflow1
+cd tensorflow1
+
+git clone https://github.com/tensorflow/models.git
+cd C:\tensorflow1\models\research\object_detection
+
+curl http://download.tensorflow.org/models/object_detection/faster_rcnn_inception_v2_coco_2018_01_28.tar.gz -O faster_rcnn_inception_v2_coco_2018_01_28.tar.gz
+"C:\Program Files\7-Zip\7z.exe" x  faster_rcnn_inception_v2_coco_2018_01_28.tar.gz  -o./ -y -r
+"C:\Program Files\7-Zip\7z.exe" x  faster_rcnn_inception_v2_coco_2018_01_28.tar  -o./ -y -r
+del faster_rcnn_inception_v2_coco_2018_01_28.tar.gz
+del faster_rcnn_inception_v2_coco_2018_01_28.tar
+
+SET src_folder=c:\tensorflow1.5
+SET tar_folder=c:\tensorflow1\models\research\object_detection
+for /f "delims=|" %%a IN ('dir "%src_folder%" /b') do move /Y %src_folder%\%%a %tar_folder%
+copy c:\tensorflow1\models\research\object_detection\protos_missing\*.* c:\tensorflow1\models\research\object_detection\proto
+
+call conda create -y -n tensorflow1 pip python=3.5
+
+cd c:\tensorflow1\models\research\object_detection
+activate tensorflow1
+
