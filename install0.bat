49996bc99bc601ed102d5dc9c47016bef494ef4d
martinjiang8 <58094948+martinjiang8@users.noreply.github.com>
2019-11-25 14:52:30 -0500

    minor

--- install0.bat
+++ install0.bat
@@ -18,4 +18,9 @@ SET src_folder=c:\tensorflow1.5\r1.5_add_on
 SET tar_folder=c:\tensorflow1\models\research\object_detection
 for /f "delims=|" %%a IN ('dir "%src_folder%" /b') do move /Y %src_folder%\%%a %tar_folder%
 
+copy c:\tensorflow1\models\research\object_detection\protos_missing\*.* c:\tensorflow1\models\research\object_detection\protos
 
+call conda create -y -n tensorflow1 pip python=3.5
+
+cd c:\tensorflow1
+activate tensorflow1
