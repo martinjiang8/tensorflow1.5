f3f7dddc7608880e24426894fd336bbc37fc1242
martinjiang8 <58094948+martinjiang8@users.noreply.github.com>
2019-11-25 14:43:10 -0500

    major

---
+++ install1.bat
@@ -0,0 +1,6 @@
+copy c:\tensorflow1\models\research\object_detection\protos_missing\*.* c:\tensorflow1\models\research\object_detection\protos
+
+call conda create -y -n tensorflow1 pip python=3.5
+
+cd c:\tensorflow1\models\research\object_detection
+activate tensorflow1
---
+++ set_path1.bat
@@ -0,0 +1,4 @@
+set PYTHONPATH=C:\tensorflow1\models;C:\tensorflow1\models\research;C:\tensorflow1\models\research\slim
+cd C:\tensorflow1\models\research\object_detection
+
+activate tensorflow1
