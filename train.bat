511a5ad8ed09bdf9ef967117b49e6f01649f3eff
martinjiang8 <58094948+martinjiang8@users.noreply.github.com>
2019-11-25 14:27:31 -0500

    major overhall

--- train.bat
+++
@@ -1,3 +0,0 @@
-python generate_tfrecord.py --csv_input=images\train_labels.csv --image_dir=images\train --output_path=train.record
-python generate_tfrecord.py --csv_input=images\test_labels.csv --image_dir=images\test --output_path=test.record
-python train.py --logtostderr --train_dir=training/ --pipeline_config_path=training/faster_rcnn_inception_v2_pets.config
